afd9e18d3c04e8d35e240f7468c8dcce5fe1ab6a
Unknown <fabio.cesso@gmail.com>
2018-01-17 01:26:49 -0200

    feat(compression): enabling gzip compression
    Also disabling uglifly

--- config/index.js
+++ config/index.js
@@ -13,11 +13,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionGzip: true,
     productionGzipExtensions: ['js', 'css'],
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
