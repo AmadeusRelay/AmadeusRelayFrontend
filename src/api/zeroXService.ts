fa135e5adfbcf3563c43b3b07c18e636274d5dfa
Unknown <fabio.cesso@gmail.com>
2018-01-31 00:19:34 -0200

    fix(WETH/address): update WETH address

--- src/api/zeroXService.ts
+++ src/api/zeroXService.ts
@@ -9,7 +9,9 @@ export class ZeroXService {
     private zeroEx: ZeroEx;
 
     public constructor() {
-        this.zeroEx = new ZeroEx(web3.currentProvider);
+        this.zeroEx = new ZeroEx(web3.currentProvider,  {
+              "etherTokenContractAddress": "0xd0a1e359811322d97991e03f863a0c30c2cf029c"
+            });
     }
 
     public getCoinBase(): string {
