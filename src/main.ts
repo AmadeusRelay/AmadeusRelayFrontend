fadf27720cced13f61a9ec3b8cdcbcd7943f8125
Unknown <fabio.cesso@gmail.com>
2018-05-02 00:42:57 -0300

    fix(vue): error after new version

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import App from './App.vue'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
@@ -15,7 +14,6 @@ import 'codemirror/theme/base16-dark.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
-Vue.use(VueResource)
 Vue.use(VueCodemirror, { 
   options: { 
     theme: 'base16-dark', 
